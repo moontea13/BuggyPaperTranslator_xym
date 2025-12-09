663c6e75e8608a5038c7c99b38d2af5cd5c48663
moontea13 <2797579036@qq.com>
2025-12-10 01:39:04 +0800

    第二版更改 1. 完成了进度显示实现 2. 完成了断点续传实现 目前问题：当需要翻译的比较多的时候请求被路由到了额度不够的模型上导致翻译不出来，需要做一个异常处理（表现是result.csv）文件一半是翻译过的，一半没被翻译 未完成：异常处理：增加 try-except 和 tenacity（或手动）重试机制，防止因网络波动或 Rate Limit (429) 导致程序崩溃。

--- translator_legacy.py
+++ translator_legacy.py
@@ -7,6 +7,9 @@ import tqdm
 
 # 从environment中读取HF_TOKEN
 HF_TOKEN = os.getenv("HF_TOKEN", "") 
+# api_key = os.environ.get("HF_TOKEN")
+# print(f"API Key exists: {bool(api_key)}")
+# print(f"Key length: {len(api_key) if api_key else 0}")
 
 # 选用了最新的 OpenAI 兼容接口
 client = OpenAI(
@@ -25,12 +28,12 @@ def translate_text(text):  # 用来调用API翻译传入的文本
             messages=[
                 # 简单更改了prompt
                 {"role": "system", 
-                 "content": "你是一个专业翻译助手，请将用户提供的一批英文论文的标题和摘要翻译成中文。保持原意不变，语言自然流畅，术语准确。"},
+                 "content": "你是一个专业翻译助手，请将用户提供的一批英文论文的标题或摘要翻译成中文，每次输出为一段无格式、无换行的文字。保持原意不变，语言自然流畅，术语准确。"},
                 {"role": "user", 
                  "content": f"请翻译以下文本：\n{text}"}
             ],
             temperature=0.2,
-            max_tokens=len(text) * 3,
+            max_tokens=len(text) * 2,
         )
         return response.choices[0].message.content
     except Exception as e:
@@ -40,18 +43,29 @@ def translate_text(text):  # 用来调用API翻译传入的文本
 
 def main():
     # 读取数据
-    df = pd.read_csv("papers.csv")
+    # df = pd.read_csv("papers.csv")
+    df = pd.read_csv("iccv2025.csv")
+
+    if not os.path.exists("result.csv"):
+        with open("result.csv", "w", encoding="utf-8") as f:
+            f.write("title,authors,abstract,date,paper_url,score,title_cn,abstract_cn\n") 
 
     # 遍历每一行，翻译摘要
-    for index, row in df.iterrows():
+    for index, row in tqdm.tqdm(df.iterrows(), total=len(df), desc="翻译进度"):
         abstract = row['abstract']
-        print(f"Translating paper {row['id']}...")
+        title = row['title']
+        print(f"Translating paper {row['title']}...")
 
-        cn_abstract = translate_text(abstract)
+        try:
+            title_cn = translate_text(title)
+            abstract_cn = translate_text(abstract)
+        except Exception as e:
+            print(f"翻译失败: {e}")
+            continue  # 跳过这一条，继续下一个
 
         # 将结果写入新文件
         with open("result.csv", "a", encoding="utf-8") as f: # 改为用a模式写入
-            f.write(f"{row['id']},{cn_abstract}\n")
+            f.write(f"{title},{row['authors']},{abstract},{row['date']},{row['paper_url']},{row['score']},{title_cn},{abstract_cn}\n")
         time.sleep(2)  # 避免请求过于频繁 更改了时间长度
 
 
